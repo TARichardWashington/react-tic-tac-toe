03c628f71d89fffa0ba525e6944ef7b5e3af639b
Richard Washington <richard.washington@taxassist.co.uk>
2023-03-11 15:32:12 +0000

    :sparkles: Add fragment to allow returning of two elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-
-
 export default function Square() {
-  return <button className="square">X</button>;
+  return <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </>;
 }
