80200c26d1e41a17f0281c15b160a2e259058f94
Richard Washington <richard.washington@taxassist.co.uk>
2023-03-11 15:32:39 +0000

    :sparkles: Add 9 buttons - in a row though!

--- src/App.js
+++ src/App.js
@@ -2,5 +2,12 @@ export default function Square() {
   return <>
     <button className="square">X</button>
     <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
   </>;
 }
