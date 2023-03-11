f6cb470a4ee63d1e6535e80dc53a5867d8684a23
Richard Washington <richard.washington@taxassist.co.uk>
2023-03-11 16:11:24 +0000

    :sparkles:  Enable x going to the first Square when any are clicked

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import { useState } from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square({ value, onSquareCLick }) {
+  return <button className="square" onClick={onSquareCLick}>{value}</button>;
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = 'x';
+    setSquares(nextSquares);
+  }
+
   return <>
     <div className="board-row">
-      <Square value={squares[0]} />
-      <Square value={squares[1]} />
-      <Square value={squares[2]} />
+      <Square value={squares[0]} onSquareCLick={handleClick} />
+      <Square value={squares[1]} onSquareCLick={handleClick} />
+      <Square value={squares[2]} onSquareCLick={handleClick} />
     </div>
 
     <div className="board-row">
-      <Square value={squares[3]} />
-      <Square value={squares[4]} />
-      <Square value={squares[5]} />
+      <Square value={squares[3]} onSquareCLick={handleClick} />
+      <Square value={squares[4]} onSquareCLick={handleClick} />
+      <Square value={squares[5]} onSquareCLick={handleClick} />
     </div>
 
     <div className="board-row">
-      <Square value={squares[6]} />
-      <Square value={squares[7]} />
-      <Square value={squares[8]} />
+      <Square value={squares[6]} onSquareCLick={handleClick} />
+      <Square value={squares[7]} onSquareCLick={handleClick} />
+      <Square value={squares[8]} onSquareCLick={handleClick} />
     </div>
   </>;
 }
