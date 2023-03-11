74652991f344714a786fb0dcdc6d053b56443a98
Richard Washington <richard.washington@taxassist.co.uk>
2023-03-11 15:47:48 +0000

    :art: Add semi-colon for clarity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function Square() {
-  const [value, setValue] = useState(null)
+  const [value, setValue] = useState(null);
 
   function handleClick() {
     setValue('x');
